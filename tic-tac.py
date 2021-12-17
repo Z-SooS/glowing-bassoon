891b8fdf398c69c949bf31c7a73a2f9ccd5b65f0
Z-SooS <zoli.soos98@gmail.com>
2021-12-17 12:31:03 +0100

    add mark,get_player function

--- tic-tac.py
+++ tic-tac.py
@@ -2,66 +2,104 @@ import random
 
 def init_board():
     return [[".",".","."],[".",".","."],[".",".","."]]
-
-def get_input():
+def get_move(board):
     inp=input("Input:").upper()
     if len(inp)==2:
         if inp[0]=='A':
             if inp[1]=="1":
-                return (0,0)
+                if board[0][0]!='.':
+                    print("Already occupied")
+                    return get_move(board)
+                else:
+                    return (0,0)
             elif inp[1]=="2":
-                return (0,1)
+                if board[0][1]!='.':
+                    print("Already occupied")
+                    return get_move(board)
+                else:
+                    return (0,1)    
             elif inp[1]=="3":
-                return (0,2)
+                if board[0][2]!='.':
+                    print("Already occupied")
+                    return get_move(board)
+                else:
+                    return (0,2)
             else:
                 print("Incorrect format!")
-                return get_input()
+                return get_move(board)
         if inp[0]=='B':
             if inp[1]=="1":
-                return (1,0)
+                if board[1][0]!='.':
+                    print("Already occupied")
+                    return get_move(board)
+                else:
+                    return (1,0)
             elif inp[1]=="2":
-                return (1,1)
+                if board[1][1]!='.':
+                    print("Already occupied")
+                    return get_move(board)
+                else:
+                    return (1,1)
             elif inp[1]=="3":
-                return (1,2)
+                if board[1][2]!='.':
+                    print("Already occupied")
+                    return get_move(board)
+                else:
+                    return (1,2)
             else:
                 print("Incorrect format!")
-                return get_input()
+                return get_move(board)
         if inp[0]=='C':
             if inp[1]=="1":
-                return (2,0)
+                if board[2][0]!='.':
+                    print("Already occupied")
+                    return get_move(board)
+                else:
+                    return (2,0)
             elif inp[1]=="2":
-                return (2,1)
+                if board[2][1]!='.':
+                    print("Already occupied")
+                    return get_move(board)
+                else:
+                    return (2,1)
             elif inp[1]=="3":
-                return (2,2)
+                if board[2][2]!='.':
+                    print("Already occupied")
+                    return get_move(board)
+                else:
+                    return (2,2)
             else:
                 print("Incorrect format!")
-                return get_input()
+                return get_move(board)
         else:
             print("Incorrect format!")
-            return get_input()
+            return get_move(board)
     else:
         print("Incorrect lenght!")
-        return get_input()
-
-def get_move(board):
-    inp=get_input()
-    if board[inp[0]][inp[1]]!='.':
-        print("Already occupied")
         return get_move(board)
-    else:
-        board[inp[0]][inp[1]]="X"
-        return board
-
+def mark(board,move,player):
+    board[move[0]][move[1]]=player
+    return board
 def display_board(board):
     print("# 1 | 2 | 3 ")
     print(f"A {board[0][0]} | {board[0][1]} | {board[0][2]}")
     print(f"B {board[1][0]} | {board[1][1]} | {board[1][2]}")
     print(f"C {board[2][0]} | {board[2][1]} | {board[2][2]}")
     pass
+def get_player():
+    player=input("What player would you like to be?(X,O)?").upper()
+    return charcheck(player)
+def charcheck(player):
+    if len(player)==1 and (player=="X" or player=="O"):
+        return player
+    else:
+        print("Incorrect character!")
+        return get_player()
 
 
 board=init_board()
-#print(init_board())
+player="X"
 display_board(board)
-board=get_move(board)
+move=get_move(board)
+board=mark(board,move,player)
 display_board(board)
